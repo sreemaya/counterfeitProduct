3c8ff2f64fd721b4dd4632bb5e91f73944b36326
Sharminas V P <sharmisagir@gmail.com>
2023-03-10 00:28:15 +0530

    manufacture

--- counterfiet.py
+++ counterfiet.py
@@ -10,7 +10,6 @@ app.secret_key="block"
 
 
 
-
 @app.route('/view_com')
 def view_com():
     if session['lg'] == "lin":
@@ -30,6 +29,32 @@ def view_comp():
         return render_template("Admin/View_complaints.html", data=q)
     else:
         return redirect('/')
+# ===================================================manifacture===========================================================
+
+@app.route('/reg',methods=['post','get'])
+def reg():
+    if request.method=='POST':
+        n1=request.form['n1']
+        n2=request.form['n2']
+        n3=request.form['n3']
+        n4=request.form['n4']
+        n5=request.form['n5']
+        n6=request.form['n6']
+        n7=request.form['n7']
+        n8=request.form['n8']
+        n9=request.form['n9']
+        n10=request.form['n10']
+        db=Db()
+        q2=db.selectOne("select * from login where user_name='"+n8+"'")
+        if q2 is None:
+            q=db.insert("insert into `login`(`login_id`,`user_name`,`password`,`user_type`) values ( NULL,'"+n8+"','"+n10+"','pending');")
+            q1=db.insert("insert into `company`(`ins_id`,`ins_name`,`ins_license`,`ins_establishedyr`,`ins_place`,`ins_post`,`ins_pin`,`ins_district`,`ins_ph`,`ins_email`) values ( '"+str(q)+"','"+n1+"','"+n2+"','"+n3+"','"+n4+"','"+n5+"','"+n7+"','"+n6+"','"+n9+"','"+n8+"');")
+            return ''' <script> alert("Registered sucessfully...!!!!");window.location='/' </script>   '''
+
+        else:
+            return ''' <script> alert("USer already found...!!!!");window.location='/reg' </script>   '''
+
+    return render_template("Manufacture/reg.html")
 
 
 if __name__ == '__main__':
