4a5d608b1561427a6bb17e13b30677e1d47b5424
Rahul-BP <rahulzantoz10@lbscek.ac.in>
2023-03-10 00:36:26 +0530

    manufacture

--- counterfiet.py
+++ counterfiet.py
@@ -34,6 +34,36 @@ def view_user():
         return redirect('/')
 
 
+# ===================================================manifacture===========================================================
+
+
+@app.route('/product_view', methods=['post','get'])
+def product_view():
+    if session['lg'] == "lin":
+        data=[]
+        with open(compiled_contract_path) as file:
+            contract_json = json.load(file)  # load contract info as JSON
+            contract_abi = contract_json['abi']  # fetch contract's abi - necessary to call its functions
+        contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi)
+        blocknumber = web3.eth.get_block_number()
+        #print(blocknumber)
+        for i in range(blocknumber, 0, -1):
+            a = web3.eth.get_transaction_by_block(i, 0)
+            decoded_input = contract.decode_function_input(a['input'])
+            # print(decoded_input)
+            res={}
+            res['productname']=decoded_input[1]['p']
+            res['location']=decoded_input[1]['l']
+            res['timestamp']=decoded_input[1]['t']
+            res['ins']=decoded_input[1]['ins']
+            res['details']=decoded_input[1]['d']
+            res['mid']=decoded_input[1]['mid']
+            if str(decoded_input[1]['mid'])==str(session['lid']):
+                data.append(res)
+
+        return render_template("Manufacture/product_view.html", data=data)
+    else:
+        return redirect('/')
 
 
 
