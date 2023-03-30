ee9e70b6f17e8e59e325aad90e1441e9abffdb04
sudarshkm05 <sudarshkm85@lbscek.ac.in>
2023-03-30 23:39:32 +0530

    v3

---
+++ android/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,102 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if(id==R.id.nav_home){
+            Intent ij= new Intent(getApplicationContext(),View_man.class);
+            startActivity(ij);
+
+        }
+
+        if(id==R.id.nav_gallery){
+            Intent ij= new Intent(getApplicationContext(),Scan_rfid.class);
+            startActivity(ij);
+
+        }
+
+        if(id==R.id.nav_slideshow){
+
+            Intent i = new Intent(getApplicationContext(), Comp.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        }
+
+        if(id==R.id.lg){
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        }
+        return false;
+    }
+}
---
+++ android/app/src/main/java/com/example/myapplication/View_prod.java
@@ -0,0 +1,109 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class View_prod extends AppCompatActivity {
+    ListView li;
+    SharedPreferences sh;
+    String url1,url;
+    String[] s1,s2,s3,s4,s5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_prod);
+        sh= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        url1=sh.getString("url","");
+        url=url1+"viewprd";
+        li=findViewById(R.id.li);
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+//                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            if (jsonObj.getString("status").equalsIgnoreCase("ok")) {
+//view service code
+                                JSONArray js= jsonObj.getJSONArray("data");//from python
+//                                Toast.makeText(Viewfeedback.this, ""+js.toString(), Toast.LENGTH_SHORT).show();
+                                s1=new String[js.length()];
+                                s2=new String[js.length()];
+                                s3=new String[js.length()];
+                                s4=new String[js.length()];
+                                s5=new String[js.length()];
+
+                                for(int i=0;i<js.length();i++) {
+                                    JSONObject u = js.getJSONObject(i);
+                                    s1[i] = u.getString("productname");//dbcolumn name
+                                    s2[i] = u.getString("location");//dbcolumn name
+                                    s3[i] = u.getString("timestamp");
+                                    s4[i] = u.getString("ins");
+                                    s5[i] = u.getString("details");
+
+                                }
+                                li.setAdapter(new Custom_p(getApplicationContext(),s1,s2,s3,s4,s5));//custom_view_service.xml and li is the listview object
+                            }
+
+
+                            // }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Not found", Toast.LENGTH_LONG).show();
+                            }
+
+                        }    catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(getApplicationContext(), "eeeee" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ) {
+
+            //                value Passing android to python
+            @Override
+            protected Map<String, String> getParams() {
+                SharedPreferences sh = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("mid",sh.getString("mid",""));
+                return params;
+            }
+        };
+
+
+        int MY_SOCKET_TIMEOUT_MS=100000;
+
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(
+                MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(postRequest);
+
+    }
+}
